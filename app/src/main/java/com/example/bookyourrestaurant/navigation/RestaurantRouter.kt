0ac4f9a0b734ee388fe7f038d7eb18f32be8ae05
Jehad1965 <55837928+Jehad1965@users.noreply.github.com>
2025-01-15 22:51:27 +0000

    I add the screen to the Restaurant navigator

--- app/src/main/java/com/example/bookyourrestaurant/navigation/RestaurantRouter.kt
+++ app/src/main/java/com/example/bookyourrestaurant/navigation/RestaurantRouter.kt
@@ -6,13 +6,17 @@ import androidx.compose.runtime.mutableStateOf
 sealed class  Screen(){
     data object SignUpScreen : Screen()
     data object  TermsAndConditionsScreen : Screen()
-    data object LoginScreen : Screen()
-    data object  HomeScreen :Screen()
-    data object SplashScreen : Screen()
-    data object  UserProfile :Screen()
+    data object   LoginScreen : Screen()
+    data object   Home :Screen()
+    data object   SplashScreen : Screen()
+    data object   UserProfile :Screen()
+    data object   Restaurant : Screen()
+    data object ForgotPasswordScreen : Screen()
+
+
 }
 object RestaurantRouter{
-    var currentScreen: MutableState<Screen> = mutableStateOf(Screen.SplashScreen)
+    var currentScreen: MutableState<Screen> = mutableStateOf(Screen.Home)
 
     fun navigateTo(destination: Screen){
         currentScreen.value = destination
